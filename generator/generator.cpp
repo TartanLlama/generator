2299f54197d6ebbba5485dc154e0a260863426f9
Sy Brand <tartanllama@gmail.com>
2021-03-24 14:05:51 +0000

    Add project files.

---
+++ generator/generator.cpp
@@ -0,0 +1,12 @@
+﻿// generator.cpp : Defines the entry point for the application.
+//
+
+#include "generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
